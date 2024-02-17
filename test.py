419b159d603d32d235cb29a712407fce81a51519
AmazingAndrewM1 <michaea3@tcnj.edu>
2024-02-17 15:34:33 -0500

    Adding this to GitHub as a better input parser.

--- test.py
+++ test.py
@@ -1,42 +1,36 @@
-def parser(expression):
+def tokenizer(expression):
     tokens = []
     
     conditional_flag = 0
     biconditional_flag = 0
     for character in expression:
-        if character.islower() or character.isupper():
-            tokens.append('VAR')
-        elif character == '!':
-            tokens.append('!')
-        elif character == '|':
-            tokens.append('|')
-        elif character == '&':
-            tokens.append('&')
-        elif character == '(':
-            tokens.append('(')
-        elif character == ')':
-            tokens.append(')')
-        elif character == '=' and conditional_flag == 0:
-            conditional_flag = 1
-        elif character == '>' and conditional_flag == 1:
-            tokens.append('=>')
-            conditional_flag = 0
-        elif character == '<' and biconditional_flag == 0:
+        if character == '<' and biconditional_flag == 0:
             biconditional_flag = 1
         elif character == '=' and biconditional_flag == 1:
             biconditional_flag = 2
         elif character == '>' and biconditional_flag == 2:
             tokens.append('<=>')
             biconditional_flag = 0
+        elif character == '=' and conditional_flag == 0:
+            conditional_flag = 1
+        elif character == '>' and conditional_flag == 1:
+            tokens.append('=>')
+            conditional_flag = 0
+        elif conditional_flag != 0 or biconditional_flag != 0:
+            raise Exception(f"Invalid Syntax")
+        elif character.islower() or character.isupper() or character in "!|&()":
+            tokens.append(character)
         elif character.isspace():
             pass
         else:
             raise Exception(f"Invalid Character Input: {character}")
+    if conditional_flag != 0 or conditional_flag != 0:
+        raise Exception(f"Invalid Syntax")
     return tokens
 
 
 def main():
-    tokens = parser("(A & b) <= c")
+    tokens = tokenizer("(A & b) <=> c t")
     print(tokens)
 
 if __name__ == "__main__":
