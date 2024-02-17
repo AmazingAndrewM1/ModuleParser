5286762f02bc599fd486cad1cd70dc524e03b161
AmazingAndrewM1 <michaea3@tcnj.edu>
2024-02-17 12:57:01 -0500

    We have another folder to use any arbitrary HTML file.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+
 @app.route("/")
 def index():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html", title="Truth Table Solver", username="Test")
