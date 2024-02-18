1131e0aa196b18a71cd9e0643b113d29d5f83de9
AmazingAndrewM1 <michaea3@tcnj.edu>
2024-02-18 10:43:42 -0500

    I have created the Flask app to have the CSS integrated.

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="staticFiles")
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="Truth Table Solver", username="Test")
+    return render_template("Hackathon_2024.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
