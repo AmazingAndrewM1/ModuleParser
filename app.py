28fcfe999e9da5824d2fa8cbe52b215950843533
AmazingAndrewM1 <michaea3@tcnj.edu>
2024-02-26 13:46:15 -0500

    The server takes in the input and tries to generate the corresponding truth table.

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
+import savy_parser
 
 app = Flask(__name__, static_folder="staticFiles")
 
 @app.route("/")
 def index():
     return render_template("Hackathon_2024.html")
+
+@app.route("/logic", methods=["POST"])
+def logic():
+    if request.method == "POST":
+        userInput = request.form["userInput"]
+        try:
+            parseObject = savy_parser.Parser(userInput)
+            print(parseObject.truth_table)
+        except (Exception, ValueError):
+            print("An error occurred.")
+        return render_template("Hackathon_2024.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
